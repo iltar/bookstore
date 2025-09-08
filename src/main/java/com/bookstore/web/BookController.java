15e2b0c4aafa0690a59df9cc32eb8ea6e739f0ba
iltar <ilta.riikonen@gmail.com>
2025-09-08 11:15:55 +0300

    tehtävä 2.5

---
+++ src/main/java/com/bookstore/web/BookController.java
@@ -0,0 +1,16 @@
+package com.bookstore.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class BookController {
+    
+    @GetMapping("/index")
+    public String getMethodName(Model model) {
+        return "index"; //index.html
+    }
+    
+}
