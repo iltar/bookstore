9319d5eaae3eccc7652f8cee8bfe44ad9dc6bc5f
iltar <ilta.riikonen@gmail.com>
2025-09-14 21:09:43 +0300

    3.2

--- src/main/java/com/bookstore/web/BookController.java
+++ src/main/java/com/bookstore/web/BookController.java
@@ -3,15 +3,29 @@ package com.bookstore.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.bookstore.domain.BookRepository;
 
 
 @Controller
 public class BookController {
+    private BookRepository repository;
+	public BookController(BookRepository repository) {
+		this.repository = repository;
+	}
+
     
     //http://localhost:8080/index
     @GetMapping("/index")
-    public String getMethodName(Model model) {
+    public String getFrontPage(Model model) {
         return "index"; //index.html
     }
-    
+
+    //http://localhost:8080/booklist
+    @RequestMapping(value= {"/", "/booklist"})
+    public String getBooks(Model model) {
+        model.addAttribute("books", repository.findAll());
+        return "booklist"; //booklist.html
+    }
 }
